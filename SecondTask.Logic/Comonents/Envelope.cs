5d54624dc39a7efb7180eb4de30434b31f1862e9
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-04 12:03:56 +0100

    SecondTask.Logic was changed. Class Envelope was added into folder Components

---
+++ SecondTask.Logic/Comonents/Envelope.cs
@@ -0,0 +1,48 @@
+﻿namespace SecondTask.Logic.Comonents
+{
+    public class Envelope
+    {
+        public double Width { get; private set; }
+        public double Height { get; private set; }
+
+        public Envelope(double envelopeWidth, double envelopeHeight) 
+        {
+            Width = envelopeWidth;
+            Height = envelopeHeight;
+        }
+
+        /// <summary>
+        ///     Use it when you want to check if an envelope would fit in current envelope or not
+        /// </summary>
+        /// <returns>
+        ///     Return true if comparisonEnvelope can fit in current envelope and return false if cannot
+        /// </returns>
+        public bool Compare(Envelope comparisonEnvelope) 
+        {
+            if (Width > comparisonEnvelope.Width && Height > comparisonEnvelope.Height
+                || Height > comparisonEnvelope.Width && Width > comparisonEnvelope.Height) 
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        ///     Use it when you want to check if an envelope would fit in current envelope or not
+        /// </summary>
+        /// <returns>
+        ///     Return true if sides of comparisonEnvelope can fit in current envelope and return false if cannot
+        /// </returns>
+        public bool Compare(double comparisonWidth, double comparisionHeight) 
+        {
+            if ((Width > comparisonWidth && Height > comparisionHeight)
+                || (Height > comparisonWidth && Width > comparisionHeight))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
