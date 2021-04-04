b57dd40987c7d7dbaaaffbe0d718151b37858587
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-04 12:30:08 +0100

    SecondTask.Logic was changed. Abstract class View was added in the path UserInterface/Abstracts

---
+++ SecondTask.Logic/UserInterface/Abstracts/View.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SecondTask.Logic.UserInterface.Abstracts
+{
+    public abstract class View
+    {
+        public Model ModelToView { get; }
+
+        public View(Model modelToView) 
+        {
+            ModelToView = modelToView;
+        }
+
+        public abstract void Display(string message);
+    }
+}
