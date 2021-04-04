3d3f536f94357cb4bc1dff14cfe83997ce96ef9a
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-04 12:54:54 +0100

    SecondTask was changed. Class EnvelopeView was added in the path Views

---
+++ SecondTask/Views/EnvelopeView.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+using SecondTask.Logic.UserInterface.Abstracts;
+
+namespace SecondTask.Views
+{
+    public class EnvelopeView : View
+    {
+        public EnvelopeView(Model viewModel) : base(viewModel) 
+        {        
+        }
+
+        public override void Display(string message)
+        {
+            WriteLine(message);
+            WriteLine();
+        }
+    }
+}
