13bca8ab8369b1a000ca304ceb96f204faf4d36c
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-04 12:39:32 +0100

    SecondTask.Logic was changed. Abstract class Controller was added in the path UserInterface/Abstracts

---
+++ SecondTask.Logic/UserInterface/Abstracts/Controller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using SecondTask.Logic.Comonents;
+
+namespace SecondTask.Logic.UserInterface.Abstracts
+{
+    public abstract class Controller
+    {
+        public View ViewToDisplay { get; }
+
+        public Controller(View viewToDisplay) 
+        {
+            ViewToDisplay = viewToDisplay;
+        }
+
+        public abstract void SetEnvelop(Envelope newEnvelope);
+        public abstract bool CompareEnvelopes(Envelope comparisonEnvelope);
+        public abstract bool CompareEnvelopes(double comparisonWidth, double comparisonHeight);
+        public abstract void Display(string message);
+    }
+}
