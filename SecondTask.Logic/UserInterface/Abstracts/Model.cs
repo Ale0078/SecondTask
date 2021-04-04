1f8a3e56dd043c0566e5082a829580aae0273b8c
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-04 12:26:10 +0100

    SecondTask.Logic was changed. Abstract class Model was added in the path UserInterface/Abstracts

---
+++ SecondTask.Logic/UserInterface/Abstracts/Model.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using SecondTask.Logic.Comonents;
+
+namespace SecondTask.Logic.UserInterface.Abstracts
+{
+    public abstract class Model
+    {
+        public Envelope EnvelopeToCompare { get; private set; }
+
+        public Model(Envelope envelopeToCompare) 
+        {
+            EnvelopeToCompare = envelopeToCompare;
+        }
+
+        public abstract void SetnewEnvelope(Envelope newEnvelope);
+    }
+}
