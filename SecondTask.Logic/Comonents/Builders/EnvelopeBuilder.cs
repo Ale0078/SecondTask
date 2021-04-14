2321eed7d44232091d0a7ead9e279bfb46df3bef
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-14 19:18:41 +0100

    SecondTask.Logic was changed. Сlass EnvelopeBuilder was added to the path Components/Builders

---
+++ SecondTask.Logic/Comonents/Builders/EnvelopeBuilder.cs
@@ -0,0 +1,18 @@
+﻿using SecondTask.Logic.Comonents.Interfaces;
+using SecondTask.Logic.Comonents.Builders.Abstracts;
+
+namespace SecondTask.Logic.Comonents.Builders
+{
+    public class EnvelopeBuilder : Builder
+    {
+        public EnvelopeBuilder(double envelopeWidth, double envelopeHeight) :
+            base(envelopeWidth, envelopeHeight)
+        {         
+        }
+
+        public override IEnvelope Create() 
+        {
+            return new Envelope(EnvelopeWidth, EnvelopeHeight);
+        }
+    }
+}
