f5dca4c1af23d62582825a8299df85cf84369a3b
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-14 18:03:04 +0100

    SecondTask.Logic was changed. Abstrat class Builder was added to the path Components/Builders/Abstratcs

---
+++ SecondTask.Logic/Comonents/Builders/Abstracts/Builder.cs
@@ -0,0 +1,18 @@
+﻿using SecondTask.Logic.Comonents.Interfaces;
+
+namespace SecondTask.Logic.Comonents.Builders.Abstracts
+{
+    public abstract class Builder
+    {
+        public double EnvelopeWidth { get; }
+        public double EnvelopeHeight { get; }
+
+        public Builder(double envelopeWidth, double envelopeHeight) 
+        {
+            EnvelopeWidth = envelopeWidth;
+            EnvelopeHeight = envelopeHeight;
+        }
+
+        public abstract IEnvelope Create();
+    }
+}
